9bfe1828e772db483d8d69ae78ba14155f6a1b78
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-03 09:47:29 +0200

    Add files via upload
    this is question for calculating simple interest

---
+++ question1.java
@@ -0,0 +1,13 @@
+class SI {
+    public static float multp(int P, int T, int R) {
+        return (P * T * R) / 100;
+    }
+
+    class interest {
+        public static void main(String args[]) {
+            SI mySI = new SI();
+            float result = mySI.multp(100000, 5, 5);
+            System.out.println("INTEREST IS: " + result);
+        }
+    }
+}
