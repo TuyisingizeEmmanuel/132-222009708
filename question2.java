1dc154dc503a43dc18ab013d435062eb9a4da8b3
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-07 07:40:49 +0200

    Update question2.java

--- question2.java
+++ question2.java
@@ -4,7 +4,7 @@ class CI {//CLASS declaration
     }
 
     class Main {//class declaration
-        public static void main(String args[]) {//
+        public static void main(String args[]) {//main method > this is mandatory statement
             CI myCI = new CI();// object creation
             double result = myCI.multp(500000, 18, 3, 12);//call multp method and assign values for arguments
             System.out.println("RESULT IS: " + result);// code to be executed
