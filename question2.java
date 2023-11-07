33eabadbff587b248486420060f733c7496b5136
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-07 07:40:02 +0200

    Update question2.java

--- question2.java
+++ question2.java
@@ -1,13 +1,13 @@
-class CI {
-    public static double multp(double P, double R, double T, double n) {
-        return P * (Math.pow((1 + R / 1200), n * T));
+class CI {//CLASS declaration
+    public static double multp(double P, double R, double T, double n) {//method declaration
+        return P * (Math.pow((1 + R / 1200), n * T));//result to be returned by the method
     }
 
-    class Main {
-        public static void main(String args[]) {
-            CI myCI = new CI();
-            double result = myCI.multp(500000, 18, 3, 12);
-            System.out.println("RESULT IS: " + result);
+    class Main {//class declaration
+        public static void main(String args[]) {//
+            CI myCI = new CI();// object creation
+            double result = myCI.multp(500000, 18, 3, 12);//call multp method and assign values for arguments
+            System.out.println("RESULT IS: " + result);// code to be executed
         }
     }
 }
