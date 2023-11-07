cc58a742525b2820c7d27ea02e4a5d286eb54265
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-07 07:29:24 +0200

    Update question1.java

--- question1.java
+++ question1.java
@@ -1,13 +1,13 @@
 class SI {//class declaration
-    public static float multp(int P, int T, int R) {
-        return (P * T * R) / 100;
+    public static float multp(int P, int T, int R) {//method declaration
+        return (P * T * R) / 100;//result to be returned
     }
 
-    class interest {
-        public static void main(String args[]) {
-            SI mySI = new SI();
-            float result = mySI.multp(100000, 5, 5);
-            System.out.println("INTEREST IS: " + result);
+    class interest {//class declaration
+        public static void main(String args[]) {//main method > this is mandatory statement
+            SI mySI = new SI();//creating an object
+            float result = mySI.multp(100000, 5, 5);//call method multp and passing argument
+            System.out.println("INTEREST IS: " + result);//code to be executed
         }
     }
 }
