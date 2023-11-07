cf9f160c3499279305561c6bc257e5ec01965448
TuyisingizeEmmanuel <141547023+TuyisingizeEmmanuel@users.noreply.github.com>
2023-11-07 07:42:07 +0200

    Add files via upload

---
+++ c.java
@@ -0,0 +1,7 @@
+class c {// class declaration
+    public static void main(String args[]) {
+        int x = 10;// variable declaration
+        System.out.println(x++);// code to be executed(printed)
+
+    }
+}
